dab89ee66aa76651fca8ff3b428acadbae69c55f
Антон <antoinedzejob@gmail.com>
2020-05-06 18:33:15 +0300

    frontend Node instance hot fix

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 'use strict';
 
 module.exports = class extends Object {
-	static deepAssign(...args){
-		return args.filter(v => v).reduce((assigned, obj, index, array) => {
-			if(index === array.length-1){
-				this.keys(obj).map(key => {
-					assigned[key] = typeof(assigned[key]) !== 'undefined' && assigned[key] !== null ? assigned[key] : obj[key];
-				});
-				return assigned;
-			}
-			obj = this.keys(assigned).length ? assigned : obj;
-			this.keys(obj).map(key => {
-				let value = obj[key], 
-					nextObj = array[index+1],
-					nextValue = nextObj && typeof(nextObj[key]) !== 'undefined' && nextObj[key] !== null ? nextObj[key] : value;
-				if(typeof(value) === 'object' && value !== null && !(value instanceof Node) && this.keys(value).length && !Array.isArray(value)){
-					assigned[key] = {...value, ...this.deepAssign(value, nextValue)};
-				} else {
-					assigned[key] = nextValue;
-	            }
-			});
-			return assigned;
-		}, {})
-	}
+  static deepAssign(...args){
+    return args.filter(v => v).reduce((assigned, obj, index, array) => {
+      if(index === array.length-1){
+        this.keys(obj).map(key => {
+          assigned[key] = typeof(assigned[key]) !== 'undefined' && assigned[key] !== null ? assigned[key] : obj[key];
+        });
+        return assigned;
+      }
+      obj = this.keys(assigned).length ? assigned : obj;
+      this.keys(obj).map(key => {
+        let value = obj[key], 
+          nextObj = array[index+1],
+          nextValue = nextObj && typeof(nextObj[key]) !== 'undefined' && nextObj[key] !== null ? nextObj[key] : value,
+          valueInstanceOfNode;
+          
+        try {
+          valueInstanceOfNode = (value instanceof Node);
+        } catch(e){
+          valueInstanceOfNode = false;
+        }
+        
+        if(typeof(value) === 'object' && value !== null && !valueInstanceOfNode && this.keys(value).length && !Array.isArray(value)){
+          assigned[key] = {...value, ...this.deepAssign(value, nextValue)};
+        } else {
+          assigned[key] = nextValue;
+              }
+      });
+      return assigned;
+    }, {})
+  }
 }
